ebdb4e5ffc82f9c7cd1895d262840ab16ae91473
Prithviraj Kadiyala <prithvirajkadiyala@gmail.com>
2020-12-26 14:39:01 -0600

    Configured PostgreSQL

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 
 const main = async () => {
@@ -13,8 +15,8 @@ const main = async () => {
 
     orm.getMigrator().up();
 
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post);
+    // const post = orm.em.create(Post, {title: 'my first post'});
+    // await orm.em.persistAndFlush(post);
 
     // const posts = await orm.em.find(Post, {});
     // console.log(posts);
@@ -23,9 +25,10 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [HelloResolver, PostResolver],
             validate: false
         }),
+        context: ()=> ({ em: orm.em })
     });
 
     apolloServer.applyMiddleware({ app });
